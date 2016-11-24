0ac5de294e1d5662e83446e0fa43e9dee8f07999
arehmandev <arehmandev@outlook.com>
2016-11-24 21:05:21 +0000

    Update docker.sh
    Fixed command

--- docker.sh
+++ docker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 docker build --no-cache -t testnodejs .
-docker run -p 5001:5001 testnodejs
+docker run --name testnodejs -d -p 5001:5001 testnodejs
 echo "localhost:5001 serving your environment"
+echo "to kill, run docker kill testnodejs"
